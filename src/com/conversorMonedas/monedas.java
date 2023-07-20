adf95e0e44a1f3e3189db1ad339d666fe324eced
Edinson Florez <edinson.florezx7@gmail.com>
2023-07-20 11:23:36 -0500

    inicializando repositorio

---
+++ src/com/conversorMonedas/monedas.java
@@ -0,0 +1,7 @@
+package com.conversorMonedas;
+
+public class monedas {
+	public static void main(String[] args) {
+		System.out.println("hoa mundo");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Edins
+ *
+ */
+module ConversorDeMonedas {
+}
